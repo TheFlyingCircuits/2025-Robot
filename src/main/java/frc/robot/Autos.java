786170f07455e26603e61c0c5c4671ef7173cb2c
Orange Robotics <orangerobotics@gmail.com>
2025-02-05 16:48:50 -0500

    Zones and auto
    Created the zones need to test and made the auto file and started an auto

---
+++ src/main/java/frc/robot/Autos.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Commands.AlignWithReef;
+import frc.robot.Reefscape.FieldElement;
+
+public class Autos extends RobotContainer{
+
+    
+    public Command rightSideAuto() {
+
+        return new SequentialCommandGroup(
+            FlyingCircuitUtils.followPath("Right Start to Back"),
+
+            new AlignWithReef(drivetrain, charlie::getRequestedFieldOrientedVelocity, FieldElement.ReefFace.FRONT).withTimeout(5), // autoscore
+
+            FlyingCircuitUtils.followPath("Right Side to Right Source")
+        );
+
+    }
+}
--- src/main/java/frc/robot/Commands/AlignWithReef.java
+++ src/main/java/frc/robot/Commands/AlignWithReef.java
@@ -13,16 +13,39 @@ import frc.robot.subsystems.drivetrain.Drivetrain;
 public class AlignWithReef extends Command{
     private Drivetrain drivetrain;
     private Supplier<ChassisSpeeds> translationController;
-    private FieldElement target;
+    private FieldElement target;\
+    private int stalk;
+
+
+    private final FieldElement[] reefFaces = {ReefFace.FRONT, ReefFace.FRONT_LEFT, ReefFace.FRONT_RIGHT, ReefFace.BACK, ReefFace.BACK_LEFT, ReefFace.BACK_RIGHT};
 
     /**
      *  @param translationController
      */
-    public AlignWithReef(Drivetrain drivetrain, Supplier<ChassisSpeeds> translationController, FieldElement target) {
+
+    public FieldElement.ReefFace getClosestReefFace() {
+
+        double closestDistance = 100000000000000.00;
+        FieldElement closestReefFace = reefFaces[0];
+        for(int i=1; i<5; i++) {
+            // distance formula HAVE NOT TESTED
+            double distance = Math.sqrt((Math.pow((drivetrain.getPoseMeters().getX() - reefFaces[i].getPose2d().getX()), 2) 
+            + Math.pow((drivetrain.getPoseMeters().getY() - reefFaces[i].getPose2d().getY()), 2)));
+
+            if (distance < closestDistance) {
+                closestReefFace = reefFaces[i];
+            }
+        }
+
+        return closestReefFace;
+    }
+
+    public AlignWithReef(Drivetrain drivetrain, Supplier<ChassisSpeeds> translationController, int stalk, int branch) {
     
         this.drivetrain = drivetrain;
         this.translationController = translationController;
         this.target = target;
+        .stalk=stalk;
 
         if (translationController != null) {
             super.addRequirements(drivetrain);
@@ -31,12 +54,10 @@ public class AlignWithReef extends Command{
 
     @Override
     public void execute() {
-        if (target == FieldElement.ReefFace.FRONT) {
+        FieldElement.ReefFace closestFace = getClosestReefFace();
+        Pose2d targetPose = closestFace.stalks[stalk].branches[branch];
 
-            Pose2d targetPose = ReefFace.FRONT.getPose2d();
-
-            Rotation2d adjustedRotation = targetPose.getRotation().rotateBy(Rotation2d.fromDegrees(180));
-            drivetrain.fieldOrientedDriveOnALine(translationController.get(), new Pose2d(targetPose.getTranslation(), adjustedRotation));
-        }
+        Rotation2d adjustedRotation = targetPose.getRotation().rotateBy(Rotation2d.fromDegrees(180));
+        drivetrain.fieldOrientedDriveOnALine(translationController.get(), new Pose2d(targetPose.getTranslation(), adjustedRotation));
     }
 }
