1fef51ef14c5375dbd2d2461c0496b61beb0e12a
Orange Robotics <orangerobotics@gmail.com>
2025-02-08 14:48:24 -0500

    Wrist code
    Wrist Code

---
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.wrist;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Wrist extends SubsystemBase{
+    private WristIO io;
+    // private WristIOInputsAutoLogged inputs;
+
+}
---
+++ src/main/java/frc/robot/subsystems/wrist/WristIO.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.wrist;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface WristIO {
+    @AutoLog
+     public class WristIOInputs {
+        
+        public double wristRpm;
+    }
+
+    // public default void updateInputs(WristIOInputs inputs) {};
+    
+}
