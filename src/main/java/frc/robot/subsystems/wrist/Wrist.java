3d1628ae6d6cdc0071a4843e1cd9466a67981789
Orange Robotics <orangerobotics@gmail.com>
2025-02-13 14:50:11 -0500

    add a space :)

--- src/main/java/frc/robot/subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -8,7 +8,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Wrist extends SubsystemBase{
+public class Wrist extends SubsystemBase {
     
     private WristIO io;
     private WristIOInputsAutoLogged inputs;
