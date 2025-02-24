ce74064e0d9b5af854921bcd0b07f8e8d5ba7d03
Orange Robotics <orangerobotics@gmail.com>
2025-02-24 16:58:16 -0500

    broken

--- src/main/java/frc/robot/subsystems/Leds.java
+++ src/main/java/frc/robot/subsystems/Leds.java
@@ -1,89 +1,88 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Seconds;
-
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.LEDConstants;
 
 public class Leds extends SubsystemBase {
-    private AddressableLED leds;
-    private AddressableLEDBuffer buffer;
-    XboxController controller;
-    LEDPattern green;
-    LEDPattern blue;
-    LEDPattern orange;
-    LEDPattern red;
     LEDPattern white;
     LEDPattern black;
     LEDPattern base;
+    Timer ledTimer = new Timer();
+    double starLEDTimes[];
 
     public Leds() {
+
+
 
         green = LEDPattern.solid(Color.kGreen);
         blue = LEDPattern.solid(Color.kBlue);
         orange = LEDPattern.solid(Color.kYellow);
-        red = LEDPattern.solid(Color.kRed);
-        white = LEDPattern.solid(Color.kWhite);
-        black = LEDPattern.solid(Color.kBlack);
 
-        leds = new AddressableLED(LEDConstants.ledPWMPort);
-        buffer = new AddressableLEDBuffer(LEDConstants.ledsPerStrip);
-
-        leds.setLength(buffer.getLength());
-
-        base = black; // led Off
-        base.applyTo(buffer);
+        public class Leds extends SubsystemBase {
         
         leds.setData(buffer);
         leds.start();
+
+        Random random = new Random();
+        
+        double starLEDTimes[] = new double[buffer.getLength()];
+        for (int i = 0; i < starLEDTimes.length; i++) {
+            starLEDTimes[i] = random.nextDouble(2);
+        }
+        this.starLEDTimes=starLEDTimes;
+        ledTimer.start();
+    }
+
+    public void setHSVFunction(int h, int s, int v) {
+        for (int i=0; i<buffer.getLength(); i++) {
+            buffer.setHSV(i, h, s, v);
+        }
+        leds.setData(buffer);
+    }
+
+    public void starLEDs(double timeInbetween, int colorHSVNum) {
+        double maxBrightnessIncrimentals = 255 / (timeInbetween/2);
+        for (int i=0; i<buffer.getLength(); i++) {
+            if (ledTimer.get() >= starLEDTimes[i]) {
+                double timeDiff = ledTimer.get() - starLEDTimes[i];
+                if(timeDiff < (timeInbetween/2)) {
+                    timeDiff = timeDiff * maxBrightnessIncrimentals;
+                    buffer.setHSV(i, colorHSVNum, (int)timeDiff, (int)timeDiff);
+                } else if (timeDiff > timeInbetween) {
+                    starLEDTimes[i] = ledTimer.get() + timeInbetween;
+                    buffer.setHSV(i, 0, 255, 255);
+                } else {
+                    double brightnesSubraction = (timeInbetween - timeDiff) * maxBrightnessIncrimentals;
+                    buffer.setHSV(i, colorHSVNum, (int)brightnesSubraction, (int)brightnesSubraction);
+                }
+            } else {
+                buffer.setHSV(i, 100, 255, 255);
+            }
+        }
+        leds.setData(buffer);
     }
 
     //mode 
     public Command defaultCommand() {
         return DriverStation.isAutonomous() ? this.runOnce(() -> {base = blue;}) : this.runOnce(() -> {base = black;}); // auto: blue, tele: off
         
+        // return DriverStation.isAutonomous() ? this.runOnce(() -> {base = blue;}) : this.runOnce(() -> {base = black;}); // auto: blue, tele: off
+        return this.run(() -> {starLEDs(2,25);});
     }
 
 
 
-    //scoirng
-    
-    public void red() { // red
-       base = red;
-    }
 
-    public void blue() { // blue
-        base = blue;
-    }
-    
-    public Command scoreCompleteCommand() { // Blink Yellow
-        LEDPattern yellow = LEDPattern.solid(Color.kYellow);
-        return this.run(() -> {base = yellow.blink(Seconds.of(0.125));}).withTimeout(1);        
-    }
-
-    public Command coralControlledCommand() { //Blink White 
-        white = LEDPattern.solid(Color.kWhite);
-        return this.run(() -> {base = white.blink(Seconds.of(0.125));}).withTimeout(1);
-    }
-
-    // Arm  
-    public Command climbAlignedCommand() { // green
-        return this.runOnce(() -> {base = green;});
-    }
-
-    public Command climbingCommand() { // orange blink
-        return this.runOnce(() -> {base = orange.blink(Seconds.of(.6));});
-    }
-
-    public Command errorCommand() { // red blink fast
+@@ -86,14 +130,18 @@ public class Leds extends SubsystemBase {
         return this.runOnce(() -> {base = red.blink(Seconds.of(0.125));});
+    }
+
+    public Command starLEDCommand() {
+        return this.run(() -> {starLEDs(2,25);});
     }
 
     public void progressBar(double percent) {
@@ -96,4 +95,9 @@ public class Leds extends SubsystemBase {
         base.applyTo(buffer);
         leds.setData(buffer);
     }
+    // @Override
+    // public void periodic() {
+    //     base.applyTo(buffer);
+    //     leds.setData(buffer);
+    // }
 }
