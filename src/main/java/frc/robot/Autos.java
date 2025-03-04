eb5d98ea499cd51e609b0e4e3816a18498a8e6f7
Orange Robotics <orangerobotics@gmail.com>
2025-03-04 14:55:21 -0500

    update variable names and clarity

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -50,30 +51,29 @@ public class Autos extends RobotContainer{
             () -> {return typeOfIntake == "Ground";});
     }
 
-    private Command intakeIfDoesntHaveCoral() {
-        boolean doesHaveCoral = placerGrabber.doesHaveCoral();
-        if (doesHaveCoral) {
-            return null;
-        }
-        return sourceIntakeCommand();
+    private Command sourceIntakeIfDoesntHaveCoral() {
+        return sourceIntakeCommand().onlyIf(placerGrabber::doesHaveCoral);
     }
     
-    private Command isInRangeOfPose() {
-        return Commands.run(() -> {System.out.println("3");}).onlyWhile(() -> !(drivetrain.getPoseMeters().getTranslation().getDistance(drivetrain.getClosestSourceSide().getLocation2d()) > 4));
+    private Command waitUntilInRangeOfSource() {
+        return new WaitUntilCommand(() -> {
+            Translation2d robotTranslation = drivetrain.getPoseMeters().getTranslation();
+            return robotTranslation.getDistance(drivetrain.getClosestSourceSide().getLocation2d()) < 1;
+        });
     }
 
     public Command rightSideAuto() {
 
         return new SequentialCommandGroup(
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_E4),
-            intake().withDeadline(isInRangeOfPose()),
-            sourceIntakeCommand(),
+            intake().withDeadline(waitUntilInRangeOfSource()),
+            sourceIntakeIfDoesntHaveCoral(),
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_D4),
-            intake().withDeadline(isInRangeOfPose()),
-            intakeIfDoesntHaveCoral(),
+            intake().withDeadline(waitUntilInRangeOfSource()),
+            sourceIntakeIfDoesntHaveCoral(),
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_C4),
-            intake().withDeadline(isInRangeOfPose()),
-            intakeIfDoesntHaveCoral(),
+            intake().withDeadline(waitUntilInRangeOfSource()),
+            sourceIntakeIfDoesntHaveCoral(),
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_B4)
         );
 
@@ -83,14 +83,14 @@ public class Autos extends RobotContainer{
 
         return new SequentialCommandGroup(
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_J4),
-            intake().withDeadline(isInRangeOfPose()),
-            intakeIfDoesntHaveCoral(),
+            intake().withDeadline(waitUntilInRangeOfSource()),
+            sourceIntakeIfDoesntHaveCoral(),
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_K4),
-            intake().withDeadline(isInRangeOfPose()),
-            intakeIfDoesntHaveCoral(),
+            intake().withDeadline(waitUntilInRangeOfSource()),
+            sourceIntakeIfDoesntHaveCoral(),
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_L4),
-            intake().withDeadline(isInRangeOfPose()),
-            intakeIfDoesntHaveCoral(),
+            intake().withDeadline(waitUntilInRangeOfSource()),
+            sourceIntakeIfDoesntHaveCoral(),
             scoreOnReefCommand(charlie::getRequestedFieldOrientedVelocity, () -> ReefBranch.BRANCH_A4)
         );
 
